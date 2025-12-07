7ba2e51107ad7fe10fa8594b53d9058ea6382dc0
Yahirzote <bygaytph@gmail.com>
2025-12-06 22:00:55 -0600

    Create Record.java

---
+++ src/main/java/mx/uamex/fi/paradigmas/model/data/Record.java
@@ -0,0 +1,41 @@
+package mx.uamex.fi.paradigmas.data;
+
+import java.util.Date;
+
+public class Record implements Data {
+    private Jugador jugador;
+    private String juego;
+    private Date fecha;
+
+    public Record() {}
+
+    public Record(Jugador jugador, String juego, Date fecha) {
+        this.jugador = jugador;
+        this.juego = juego;
+        this.fecha = fecha;
+    }
+
+    public Jugador getJugador() {
+        return jugador;
+    }
+
+    public void setJugador(Jugador jugador) {
+        this.jugador = jugador;
+    }
+
+    public String getJuego() {
+        return juego;
+    }
+
+    public void setJuego(String juego) {
+        this.juego = juego;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+}
